9c6bcedf30d46234820a7a6038e741ff1cf5c3a3
jacob <jacob.7050@gmail.com>
2023-03-09 22:40:47 -0800

    riute and html and listener have all been updated from the previous code to attempt to write a bit cleaner code; howerver, still does not deploy...

--- server.js
+++ server.js
@@ -1,84 +1,63 @@
 //Dependencies
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
-const util = require("util");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+
+//this check i found deletes all unused declerations, so i am testing it here.
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
 
 
-//handling async process
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-
-
-//setting up server
+//Port
 const app = express();
-const PORT = process.env.PORT || 8000;
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
+var PORT = process.env.PORT || 3001;
 
 //static middleware
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 
-// API Route for GET request
-app.get("/api/notes", function(req, res){
-    readFileAsync("./develop/db/db.json", "utf8").then(function(data){
-        notes = [].concat(JSON.parse(data))
-        res.json(notes);
-    })
+
+//Setting routes for APIs
+//This gets notes saved and joins it in db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
 });
 
 
 //API route for POST request
-app.post("/api/notes", function(req, res) {
-const note = res.body;
-readFileAsync("./develop/db/db.json", "utf8").then(function(data){
-    const notes = [].concat(JSON.parse(data));
-    note.id = notes.length + 1
-    notes.push(note);
-    return notes
-}).then(function(notes){
-    writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
-    res.json(note);
-})
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
 });
-
 
 //API route for delete request
-app.delete("api/notes/:id", function (req, res){
-    const idToDelete = parseInt(req.params.id);
-    readFileAsync("./develop/db/db.json", "utf8").then(function(data){
-        const notes = [].concat(JSON.parse(data));
-        const newNotesData = []
-        for (let i = 0; i<notes.length; i++) {
-            if(idToDelete !== notes [i].id) {
-            newNotesData.push(notes[i])
-            }
-        }
-        return newNotesData
-    }).then(function (notes) {
-        writeFileAsync("./develop/db/d.json", JSON.stringify(notes))
-        res.send('Saved!')
-    })
-});
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
 
-//HTML path
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
-});
 
+//HTML calls
+//calls home page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//call for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-get.app("/", function(req, res){
-    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
-});
-
-
-// Listener
+//Start listen
 app.listen(PORT, function () {
-    console.log("App is on PORT" + PORT)
+    console.log("App listening on PORT: " + PORT);
 });
