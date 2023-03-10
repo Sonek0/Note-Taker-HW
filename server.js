4a1d82e4bf805b5fe87bb478bfe78cf38d0cb5a4
jacob <jacob.7050@gmail.com>
2023-03-09 18:34:37 -0800

    delete function added from ln 50-65 to delete unwanted notes.

--- server.js
+++ server.js
@@ -22,10 +22,44 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-// API Route and GET request
+// API Route for GET request
 app.get("/api/notes", function(req, res){
     readFileAsync("./develop/db/db.json", "utf8").then(function(data){
         notes = [].concat(JSON.parse(data))
         res.json(notes);
     })
 });
+
+
+//API route for POST request
+app.post("/api/notes", function(req, res) {
+const note = res.body;
+readFileAsync("./develop/db/db.json", "utf8").then(function(data){
+    const notes = [].concat(JSON.parse(data));
+    note.id = notes.length + 1
+    notes.push(note);
+    return notes
+}).then(function(notes){
+    writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+    res.json(note);
+})
+});
+
+
+//API route for delete request
+app.delete("api/notes/:id", function (req, res){
+    const idToDelete = parseInt(req.params.id);
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data){
+        const notes = [].concat(JSON.parse(data));
+        const newNotesData = []
+        for (let i = 0; i<notes.length; i++) {
+            if(idToDelete !== notes [i].id) {
+            newNotesData.push(notes[i])
+            }
+        }
+        return newNotesData
+    }).then(function (notes) {
+        writeFileAsync("./develop/db/d.json", JSON.stringify(notes))
+        res.send('Saved!')
+    })
+})
